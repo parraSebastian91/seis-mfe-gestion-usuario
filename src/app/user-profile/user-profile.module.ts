fc0c9bad6c2275a0fd432cd3944f6215fa5dc611
Sebastian <parra.sebastian91@gmail.com>
2026-06-11 11:20:44 -0400

    vista de usuario ok

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -9,15 +9,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { CardComponent, CardFooterDirective, CardTitleDirective } from '../../../../shared-utils/src/public-api';
 
 @NgModule({
   declarations: [
@@ -40,8 +41,8 @@ import { MatInputModule } from '@angular/material/input';
     MatSelectModule,
     MatTooltipModule,
     ReactiveFormsModule,
-    RouterModule
-
+    RouterModule,
+    CardComponent, CardTitleDirective, CardFooterDirective
   ]
 })
 export class UserProfileModule { }
